857ae3a5ae2a6d950f8daa2402d56620cb1cd653
ojspp000@naver.com <ojspp000@naver.com>
2024-07-21 01:17:08 +0900

    pusf

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 import express from "express";
-import mongoose, { Mongoose } from "mongoose";
+import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from 'cors';
-import cookieParser from 'cookie-parser'; // cookie-parser 가져오기
+import cookieParser from 'cookie-parser';
 import authRouter from './routes/auth.js';
 import userRouter from './routes/users.js';
 import cartRouter from './routes/carts.js';
 import orderRouter from './routes/orders.js';
 import productRouter from './routes/products.js';
-const app = express();
 
+const app = express();
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI)
-  .then(app.listen(8000,console.log("Server Running on port:8000")))
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+})
+  .then(() => {
+    app.listen(8000, () => console.log("Server Running on port: 8000"));
+  })
   .catch(err => console.log(err));
+
 app.use(cors());
 app.use(express.json());
-app.use(cookieParser()); // 쿠키 파싱 미들웨어 설정
-app.use("/api/auth",authRouter);
-app.use("/api/users",userRouter);
-app.use("/api/carts",cartRouter);
-app.use("/api/orders",orderRouter);
-app.use("/api/products",productRouter);
+app.use(cookieParser());
+
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
+app.use("/api/carts", cartRouter);
+app.use("/api/orders", orderRouter);
+app.use("/api/products", productRouter);
